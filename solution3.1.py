5b7a8f519daedc79fd81f6d447c967ad9aa687b4
charlie <the_charlie_daly@hotmail.co.uk>
2016-11-05 18:11:35 +0000

    Working but untested sol for 3.1

--- solution3.1.py
+++ solution3.1.py
@@ -1,15 +1,22 @@
-def​ answer(start,​ ​length):
-    result = start
-    for group_index in range(length):
-        group_start = start + (group_index * length)
-        group_length = length - group_index
-        result ^= calc_sequence(group_start, group_length)
-    
+def answer(start, length):
+    result = 0
+    for row in range(length):
+        i = (row * length) + start 
+        result ^= xor_sum(i, i + length) ^ xor_sum(i + length - row,  i + length)
+
     return result
+
+def xor_sum(a, b):
+    """
+    XOR sum of sequence of positive ints starting at a and finishing at b.
     
-def calc_sequence(start, length):
-    result = start
-    for i in range(length):
-        result ^= (start + i)
-    
-    return result
+    Equivilent to fold using (_ ^ _) over range(a, b).
+    """
+    if a < 0 | b < 0:
+        return None
+
+    if a == 0:
+        if b % 2 == 0: return 0
+        else: return b - 1
+    else:
+        return xor_sum(0, a) ^ xor_sum(0, b)
