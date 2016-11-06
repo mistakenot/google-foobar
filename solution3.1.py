5f272749b41c085c114219e0c21743da2e25aa35
charlie <the_charlie_daly@hotmail.co.uk>
2016-11-06 13:07:22 +0000

    Submitted 3.1 and started 3.2

--- solution3.1.py
+++ solution3.1.py
@@ -1,22 +1,35 @@
 def answer(start, length):
     result = 0
     for row in range(length):
-        i = (row * length) + start 
-        result ^= xor_sum(i, i + length) ^ xor_sum(i + length - row,  i + length)
+        row_start = (row * length) + start
+        row_end = row_start + length
+        result ^= xor_sum(row_start, row_end) ^ xor_sum(row_end - row,  row_end)
 
     return result
 
 def xor_sum(a, b):
     """
-    XOR sum of sequence of positive ints starting at a and finishing at b.
+    XOR sum of sequence of positive ints between a and b inclusive.
     
-    Equivilent to fold using (_ ^ _) over range(a, b).
+    Equivilent to fold using (_ ^ _) over range a -> b.
+
+    :param a: Start of sequence. Must be >= 0.
+    :param b: End of sequence. Must be >= 0.
+    :return: returns XOR sum as int.
     """
     if a < 0 | b < 0:
-        return None
+        raise Exception("Parameters must be positive.")
+    if b < a:
+        return xor_sum(b, a)
 
     if a == 0:
-        if b % 2 == 0: return 0
-        else: return b - 1
+        if b % 4 == 0: 
+            return b
+        elif b % 4 == 1:
+            return 1
+        elif b % 4 == 2:
+            return b + 1
+        else: 
+            return 0
     else:
-        return xor_sum(0, a) ^ xor_sum(0, b)
+        return xor_sum(0, a - 1) ^ xor_sum(0, b)
