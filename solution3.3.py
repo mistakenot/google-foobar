952b3509078e02e62a35b08a63cdf2a5e9b0e3a5
charlie <the_charlie_daly@hotmail.co.uk>
2016-11-09 00:37:39 +0000

    Working solution.

--- solution3.3.py
+++ solution3.3.py
@@ -3,26 +3,24 @@ import math
 def answer(n):
     return 1
 
+qk_cache = {}
+
 def q(k):
-    # Accumulate the result
-    result = a(k)
-    # Number of loop iterations
+    if (qk_cache.has_key(k)):
+        return qk_cache[k]
+
+    result_acc = a(k)
+
     i = 0
-    # Used to control +|- flipping in sequence
-    flip = 1
-    while k >= pentagonal(i):
-        # Generates 0, 1, 1, 0, 0, 1, 1, 0, 0...
-        flip = (flip + (k % 2)) % 2
-        sign = math.pow(-1, flip)
-        pent = pentagonal(i)
-        prev = q(k - pent)
-
-        log = (k, sign, pent)
-        print(log)
-        result += (math.pow(-1, flip) * prev)
-        i += 1
-
-    return result
+    for p in pentagonal_series():
+        if p > k:
+            qk_cache[k] = result_acc
+            return result_acc
+        else:
+            sign = get_sign(i)
+            prev = q(k - p)
+            result_acc += (sign * prev)
+            i += 1
 
 def a(k):
     """
@@ -33,9 +31,9 @@ def a(k):
     m2 = (1 - math.sqrt(1 + (12*k))) / 6
 
     if math.trunc(m1) == m1:
-        return math.pow(-1, m1)
+        return int(math.pow(-1, m1))
     elif math.trunc(m2) == m2:
-        return math.pow(-1, m2)
+        return int(math.pow(-1, m2))
     else:
         return 0
 
@@ -50,11 +48,23 @@ def pentagonal(n):
     return p
 
 def pentagonal_series():
-    i = 0
-    while True:
-        yield pentagonal(i)
-        i += 1
+    try:
+        n = 0
+        while True:
+            k = math.trunc((n+2)/2) * math.pow(-1, n % 2)
+            p = k * ((3 * k) - 1) / 2
+            yield p
+            n += 1
+    except GeneratorExit:
+        return
 
-print("k", "sign", "pent")
-for k in pentagonal_series():
-    print(k)
+def get_sign(i):
+    """
+    A function that returns the ith element of the sequence:
+    1, 1, -1, -1, 1, 1, -1, -1...
+    """
+    sign = 1 - ((i % 4) - (i % 2))
+    return sign
+
+for i in range(201):
+    print(q(i))
