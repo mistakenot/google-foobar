751d6bd7a3b37b28a280852ffbcc395e0672f0f6
charlie <the_charlie_daly@hotmail.co.uk>
2016-11-09 00:49:47 +0000

    Refactoring...

--- solution3.3.py
+++ solution3.3.py
@@ -1,6 +1,17 @@
 import math
 
 def answer(n):
+    """
+    Generalised form: 
+        Find the number of partitions of the integer n with distinct parts.
+
+    https://en.wikipedia.org/wiki/Partition_(number_theory)#Odd_parts_and_distinct_parts
+
+    Solution is based the recurring expansion:
+        q(k) = ak + q(k − 1) + q(k − 2) − q(k − 5) − q(k − 7) + q(k − 12) + q(k − 15) − q(k − 22) − ...
+    Where the subtraction values (1, 2, 5, 7...) are the generalized pentagonal numbers and
+     ak is (−1)**m if k = 3m**2 − m for some integer m and is 0 otherwise.
+    """
     return 1
 
 qk_cache = {}
@@ -24,28 +35,21 @@ def q(k):
 
 def a(k):
     """
-    a(k) is (-1)^m if k = 3m^2 - m for some integer m, else 0.
-    m is calculated using quadratic formula: (-b +|- math.sqrt(b^2 - 4ac)) / 2a
+    a(k) is (-1)**m if k = 3m**2 - m for some integer m, else 0.
+    m is calculated using quadratic formula:
+        (-b +|- math.sqrt(b**2 - 4ac)) / 2a
+    for 
+        am**2 + bm + c = 0.
     """
     m1 = (1 + math.sqrt(1 + (12*k))) / 6
     m2 = (1 - math.sqrt(1 + (12*k))) / 6
 
     if math.trunc(m1) == m1:
-        return int(math.pow(-1, m1))
+        return -1**m1
     elif math.trunc(m2) == m2:
-        return int(math.pow(-1, m2))
+        return -1**m2
     else:
         return 0
-
-def pentagonal(n):
-    """
-    Iterator of the values of the pentagonal series.
-    Equal to k(3k-1)/2 for k = 1, −1, 2, −2, 3
-    e.g. 1, 2, 5, 7, 12...
-    """
-    k = math.trunc((n+2)/2) * math.pow(-1, n % 2)
-    p = k * ((3 * k) - 1) / 2
-    return p
 
 def pentagonal_series():
     try:
@@ -66,5 +70,4 @@ def get_sign(i):
     sign = 1 - ((i % 4) - (i % 2))
     return sign
 
-for i in range(201):
-    print(q(i))
+print(q(200) - 1 == 487067745)
